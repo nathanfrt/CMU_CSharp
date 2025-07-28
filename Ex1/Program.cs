89335dc356e1c547885ce53eef4e110dab2eaf08
Nathan <nathanfrt@gmail.com>
2025-07-28 20:53:19 -0300

    Fix: Exercicio 1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -4,20 +4,43 @@ namespace exercicio1
 {
     public class Program
     {
+        public static List<string> FiltraTerminadasEmA(List<string> array)
+        {
+            try
+            {
+                if (array == null || array.Count == 0)
+                {
+                    Console.WriteLine("A lista recebida é nula ou vazia.");
+                    return new List<string>();
+                }
+
+                var acentos = new[] { "á", "à", "â", "ã", "ä", "a" };
+
+                var result = array.Where(c => !string.IsNullOrWhiteSpace(c) &&
+                                        acentos.Any(d => c.EndsWith(d, StringComparison.OrdinalIgnoreCase))).ToList();
+
+                if (result.Count == 0)
+                    Console.Write("Nenhuma palavra termina com a letra 'a'.");
+
+                Console.WriteLine(string.Join(", ", result.FindAll(c => !string.IsNullOrWhiteSpace(c))));
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao filtrar palavras terminadas em 'a': {ex.Message}");
+                return new List<string>();
+            }
+        }
+
         public static void Main(string[] args)
         {
             var biblioteca = new Biblioteca();
-            Console.WriteLine("\nPalavras terminadas em A");
-            biblioteca.FiltraTerminadasEmA(new List<string> { "Pera", "Maçã", "Banana", "Uva", "Abacate" });
-
-            Console.WriteLine("\nPalavras não terminadas em A");
-            biblioteca.FiltraTerminadasEmA(new List<string> { "Caderno", "Corretivo", "Computador", "Lápis", "Marcador" });
-
-            Console.WriteLine("\nPalavras terminadas em número ou caracteres especiais");
-            biblioteca.FiltraTerminadasEmA(new List<string> { "Per$", "Maç4", "Banan@", "Uva.", "Uva", "Abacat!" });
-
-            Console.WriteLine("\nParâmetro nulo");
-            biblioteca.FiltraTerminadasEmA(new List<string>());
+            
+            FiltraTerminadasEmA(new List<string> { "Pera", "Maçã", "Banana", "Uva", "Abacate" }); // new List<string> {"Pera", "Banana", "Uva"}
+            FiltraTerminadasEmA(new List<string> { "BANANA", "AZEITE", "Sacola", "MERCADO" }); // new List<string> {"BANANA", "Sacola"} 
+            FiltraTerminadasEmA(new List<string> { "Caderno", "Corretivo", "Computador", "Lápis", "Marcador" });
+            FiltraTerminadasEmA(new List<string> { "Per$", "Maç4", "Banan@", "Uva.", "Uva", "Abacat!" });
+            FiltraTerminadasEmA(new List<string>());
         }
     }
 }
