569a17f485558496e675826b8d97a451039b7d7e
Nathan <nathanfrt@gmail.com>
2025-07-28 23:17:56 -0300

    Feat: Exercicio 4

--- Ex4/Program.cs
+++ Ex4/Program.cs
@@ -2,7 +2,6 @@
 
 namespace exercicio4
 {
-
     public class Usuario
     {
         public string Email { get; }
@@ -20,28 +19,55 @@ namespace exercicio4
     {
         public static List<Usuario> OrdenaUsuarios(List<Usuario> array)
         {
-            // Coloque seu código aqui
-            return null;
+            var users = new List<Usuario>();
+
+            try
+            {
+                if (array == null || array.Count < 1)
+                    throw new Exception("A lista não pode ser nula ou vazia");
+
+                var listaOrdenada = array.OrderBy(x => x.Nome).ToList();
+
+                foreach (var item in listaOrdenada)
+                {
+                    if (users.Any(c => c.Nome == item.Nome))
+                        throw new Exception($"Já existe usuário com o nome {item.Nome} cadastrado");
+
+                    users.Add(new Usuario(item.Email, item.Nome));
+                }
+
+                listaOrdenada.ForEach(item => Console.WriteLine($"{item.Nome}\t{item.Email}"));
+
+                return listaOrdenada;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+                return null;
+            }
         }
 
         public static void Main(string[] args)
         {
-            // Exemplos para teste. Sinta-se à vontade para completar com outros testes!
-            // Obs: Como a função retorna uma lista de objetos, a resposta abaixo apenas representa o(s) objeto(s) retornados!
-
             OrdenaUsuarios(new List<Usuario> {
                 new Usuario("jc@cmu.com.br", "João Carlos"),
                 new Usuario("ana@cmu.com.br", "Ana Maria"),
                 new Usuario("pedro@cmu.com.br", "Pedro Almeida"),
                 new Usuario("joaozin@cmu.com.br", "João Marcelo")}); // new List<Usuario> {Usuario{"Ana Maria"}, Usuario{"João Carlos"}, Usuario{"João Marcelo"}, Usuario{"Pedro Almeida"} }
 
-            OrdenaUsuarios(new List<Usuario> {}); // new List<Usuario> {}
+            OrdenaUsuarios(new List<Usuario> { }); // new List<Usuario> {}
 
             OrdenaUsuarios(new List<Usuario> {
                 new Usuario("jc@cmu.com.br", "João Carlos"),
                 new Usuario("ana@cmu.com.br", "Ana Maria"),
                 new Usuario("pedro@cmu.com.br", "Pedro Almeida"),
                 new Usuario("joaozin@cmu.com.br", "João Carlos")}); // ERRO
+
+            OrdenaUsuarios(new List<Usuario> {
+                new Usuario("joaozin@cmu.com.br", "João Carlos"),
+                new Usuario("ana@cmu.com.br", "Ana Maria"),
+                new Usuario("pedro@cmu.com.br", "Pedro Almeida"),
+                new Usuario("joaozin@cmu.com.br", "João Carlos S")}); // new List<Usuario> {Usuario{"Ana Maria"}, Usuario{"João Carlos"}, Usuario{"João Marcelo"}, Usuario{"Pedro Almeida"} }
 
             OrdenaUsuarios(null); // ERRO
         }
