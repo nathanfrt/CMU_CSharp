0a2cbb0f5a0fae70edbbb03a457f006aa5b2c639
Nathan <nathanfrt@gmail.com>
2025-07-29 14:53:42 -0300

    Fix: Exercicio 1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -6,26 +6,23 @@
         {
             try
             {
-                if (array == null || array.Count == 0)
-                {
-                    Console.WriteLine("A lista recebida é nula ou vazia.");
-                    return new List<string>();
-                }
-
+                if (array == null || array.Count == 0)                
+                    throw new Exception("A lista recebida é nula ou vazia.");                    
+                
                 var acentos = new[] { "á", "à", "â", "ã", "ä", "a" };
 
                 var result = array.Where(c => !string.IsNullOrWhiteSpace(c) &&
                                         acentos.Any(d => c.EndsWith(d, StringComparison.OrdinalIgnoreCase))).ToList();
 
                 if (result.Count == 0)
-                    Console.Write("Nenhuma palavra termina com a letra 'a'.");
+                    throw new Exception("Nenhuma palavra termina com a letra 'a'.");
 
                 Console.WriteLine(string.Join(", ", result.FindAll(c => !string.IsNullOrWhiteSpace(c))));
                 return result;
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Erro ao filtrar palavras terminadas em 'a': {ex.Message}");
+                Console.WriteLine($"ERROR: {ex.Message}");
                 return new List<string>();
             }
         }
